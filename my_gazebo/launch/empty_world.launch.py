2a16918059c22454af9457c49864339488a33999
Rogerio B. Cuenca <rogeriobcuenca@gmail.com>
2023-12-21 16:34:38 -0300

    sdf atualizado com as peças base do servo - laterais - servo - base da camera

--- my_gazebo/launch/empty_world.launch.py
+++ my_gazebo/launch/empty_world.launch.py
@@ -59,25 +59,15 @@ def generate_launch_description():
         launch_arguments={'use_sim_time': use_sim_time}.items()
     )
 
-    # spawn_turtlebot_cmd = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         os.path.join(launch_file_dir, 'spawn_turtlebot3.launch.py')
-    #     ),
-    #     launch_arguments={
-    #         'x_pose': x_pose,
-    #         'y_pose': y_pose
-    #     }.items()
-    # )
-    
-    # spawn_turtlebot_cmd = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         os.path.join(launch_file_dir, 'spawn_robot_ros2.launch.xml')
-    #     ),
-    #     launch_arguments={
-    #         'x_pose': x_pose,
-    #         'y_pose': y_pose
-    #     }.items()
-    # )
+    spawn_turtlebot_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_file_dir, 'spawn_turtlebot3.launch.py')
+        ),
+        launch_arguments={
+            'x_pose': x_pose,
+            'y_pose': y_pose
+        }.items()
+    )
     
     
     ld = LaunchDescription()
@@ -86,6 +76,6 @@ def generate_launch_description():
     ld.add_action(gzserver_cmd)
     ld.add_action(gzclient_cmd)
     ld.add_action(robot_state_publisher_cmd)
-    #ld.add_action(spawn_turtlebot_cmd)
+    ld.add_action(spawn_turtlebot_cmd)
 
     return ld
