6400e990d2ecf80a9c74448db72b0f6851f24cf7
Rogerio B. Cuenca <rogeriobcuenca@gmail.com>
2024-06-24 15:13:12 -0300

    ajuste da infra para funcionar o slam

---
+++ atualiza_infra/atualiza_infra_2024.sh
@@ -0,0 +1,4 @@
+sudo cp burger_insper.yaml /opt/ros/humble/share/turtlebot3_navigation2/param/
+sudo apt install ros-humble-rmw-cyclonedds-cpp
+echo export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp >> robotica.sh
+
