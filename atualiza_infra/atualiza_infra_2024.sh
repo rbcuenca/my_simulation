1eca34d0b356063969478eca30676f649181ecb7
Rogerio B. Cuenca <rogeriobcuenca@gmail.com>
2024-06-24 15:36:15 -0300

    acerto do atualizainfra

--- atualiza_infra/atualiza_infra_2024.sh
+++ atualiza_infra/atualiza_infra_2024.sh
@@ -1,4 +1,5 @@
 sudo cp burger_insper.yaml /opt/ros/humble/share/turtlebot3_navigation2/param/
 sudo apt install ros-humble-rmw-cyclonedds-cpp
-echo export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp >> robotica.sh
+echo export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp >> ~/robotica.sh
+./Pictures/limpeza.sh 
 
