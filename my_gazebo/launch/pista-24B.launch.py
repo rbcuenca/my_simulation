772078f5b761d0a34d368ce5142186f7b2772367
Rogerio Cuenca <rogeriobcuenca@gmail.com>
2024-11-05 17:38:53 -0300

    pacote robo funcional - nao terminado

--- my_gazebo/launch/pista-24B.launch.py
+++ my_gazebo/launch/pista-24B.launch.py
@@ -26,7 +26,8 @@ from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
-    launch_file_dir = os.path.join(get_package_share_directory('my_gazebo'), 'launch')
+    launch_file_dir_world = os.path.join(get_package_share_directory('my_gazebo'), 'launch')
+    launch_file_dir_robot = os.path.join(get_package_share_directory('insperbot'), 'launch')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
@@ -55,14 +56,14 @@ def generate_launch_description():
 
     robot_state_publisher_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(launch_file_dir, 'robot_state_publisher.launch.py')
+            os.path.join(launch_file_dir_world, 'robot_state_publisher.launch.py')
         ),
         launch_arguments={'use_sim_time': use_sim_time}.items()
     )
 
     spawn_turtlebot_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(launch_file_dir, 'spawn_turtlebot3.launch.py')
+            os.path.join(launch_file_dir_robot, 'spawn_turtlebot3.launch.py')
         ),
         launch_arguments={
             'x_pose': x_pose,
