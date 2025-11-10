c1b1eff44c104322bb52416f800173a8d7b46deb
dsoldev <dpsoler09@gmail.com>
2025-11-10 19:32:25 -0300

    Add threading and error handling to YOLO detection; update launch parameters for improved synchronization

--- gazebo_aux/launch/deteccoes.launch.py
+++ gazebo_aux/launch/deteccoes.launch.py
@@ -1,3 +1,4 @@
+# deteccoes.launch.py
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
@@ -11,17 +12,32 @@ def generate_launch_description():
                 ('image_rect', '/camera/image_raw'),
                 ('camera_info', '/camera/camera_info'),
             ],
-            parameters=[{'tag_family': 'tag36h11'}],
+            parameters=[{
+                'tag_family': 'tag36h11',
+                'approximate_sync': True,
+                'max_sync_delay': 0.3,
+                'use_sim_time': True
+            }],
         ),
         Node(
             package='ros_gz_bridge',
             executable='parameter_bridge',
-            arguments=['/camera/image_raw@sensor_msgs/msg/Image@gz.msgs.Image'],
+            name='bridge_image',
+            arguments=[
+                '/camera/image_raw@sensor_msgs/msg/Image@gz.msgs.Image',
+                '--ros-args', '--param', 'use_multithreaded_executor:=true'
+            ],
+            parameters=[{'queue_size': 10}],
         ),
         Node(
             package='ros_gz_bridge',
             executable='parameter_bridge',
-            arguments=['/camera/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo'],
+            name='bridge_info',
+            arguments=[
+                '/camera/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo',
+                '--ros-args', '--param', 'use_multithreaded_executor:=true'
+            ],
+            parameters=[{'queue_size': 10}],
         ),
         Node(
             package='gazebo_aux',
